c3ff19801d3b557a7d7a26f17a8486a5c1c44db4
Theo B. <benard320@gmail.com>
2024-04-02 11:40:04 +0200

    Started feat auth

--- mif10-backend/src/configuration.ts
+++ mif10-backend/src/configuration.ts
@@ -1,12 +1,10 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10) || 3000,
   db: {
-    type: 'postgres',
     username: process.env.DATABASE_USERNAME || "admin",
     password: process.env.DATABASE_PASSWORD || "admin",
     database: process.env.DATABASE_DB || "flightsdb",
     host: process.env.DATABASE_HOST || "192.168.75.63",
-    port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
-    synchronize: false
+    port: parseInt(process.env.DATABASE_PORT, 10) || 5432
   }
 });
