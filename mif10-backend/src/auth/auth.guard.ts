bbfad9a8afcbe7f4d8d7a5a56d5b8e3916128ab3
Theo <benard320@gmail.com>
2024-04-04 15:02:29 +0200

    wip

--- mif10-backend/src/auth/auth.guard.ts
+++ mif10-backend/src/auth/auth.guard.ts
@@ -29,8 +29,7 @@ export class AuthGuard implements CanActivate {
           secret: this.config.get("jwt.secret")
         }
       );
-      // 💡 We're assigning the payload to the request object here
-      // so that we can access it in our route handlers
+
       request['user'] = payload;
     } catch {
       throw new UnauthorizedException();
