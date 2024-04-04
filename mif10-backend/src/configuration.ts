02dd24140331243e4eec64c0213a3b3e53c35d04
seb <mendo.sebastien@etu.univ-lyon1.fr>
2024-04-04 15:40:20 +0200

    merge

--- mif10-backend/src/configuration.ts
+++ mif10-backend/src/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
   port: parseInt(process.env.PORT, 10),
   db: {
-    username: process.env.DATABASE_USERNAME || "postgres",
+    username: process.env.DATABASE_USERNAME || "",
     password: process.env.DATABASE_PASSWORD || "",
     database: process.env.DATABASE_DB || "postgres",
     host: process.env.DATABASE_HOST || "localhost",
