d940d791fd81728bb913bd527b4f187740e170ec
MFSDIOP <86809968+MFSDIOP@users.noreply.github.com>
2024-04-05 16:57:42 +0200

    Marker et Ligne

--- mif10-frontend/src/components/TestMap.vue
+++ mif10-frontend/src/components/TestMap.vue
@@ -13,6 +13,10 @@ import { fromLonLat } from 'ol/proj.js';
 import { Vector as VectorLayer } from 'ol/layer.js';
 import { Vector as VectorSource } from 'ol/source.js';
 import { Icon, Style } from 'ol/style.js';
+import { LineString } from 'ol/geom.js';
+import { Stroke} from 'ol/style.js';
+//import arc from 'arc';
+
 
 export default {
   name: 'MapContainer',
@@ -29,7 +33,9 @@ export default {
     const markerStyle = new Style({
       image: new Icon({
         anchor: [0.5, 1],
-        src: 'https://openlayers.org/en/latest/examples/data/icon.png',
+        src: './marker_icon.png',
+        width: 18,
+        eight: 18
       }),
     });
     const airportLayer = new VectorLayer({
@@ -50,10 +56,75 @@ export default {
         zoom: 2,
       }),
     });
-
+ 
     this.airports.forEach(airport => {
       this.addAirportMarker(map, airportSource, airport.lon, airport.lat, airport.name);
     });
+
+
+    const lineCoordinates = this.airports.map(airport => fromLonLat([airport.lon, airport.lat]));
+    const line = new LineString(lineCoordinates);
+
+    const lineStyle = new Style({
+      stroke: new Stroke({
+        color: 'blue',
+        width: 2,
+      }),
+    });
+
+    const lineLayer = new VectorLayer({
+      source: new VectorSource({
+        features: [new Feature({ geometry: line })],
+      }),
+      style: lineStyle,
+    });
+
+    map.addLayer(lineLayer);
+    
+    /*function generateArcLine(from, to) {
+      const arcGenerator = new arc.GreatCircle(
+        { x: from[1], y: from[0] },
+        { x: to[1], y: to[0] }
+      );
+
+      const arcLine = arcGenerator.Arc(100, { offset: 10 });
+      
+      const features = [];
+      arcLine.geometries.forEach(geometry => {
+        const line = new LineString(geometry.coords.map(coord => fromLonLat([coord[0], coord[1]])));
+        features.push(
+          new Feature({
+            geometry: line,
+            finished: true,
+          })
+        );
+      });
+
+      return features;
+    }
+
+    const arcFeatures = generateArcLine(
+      [this.airports[0].lon, this.airports[0].lat],
+      [this.airports[1].lon, this.airports[1].lat]
+    );
+
+    const lineStyle = new Style({
+      stroke: new Stroke({
+        color: 'blue',
+        width: 2,
+      }),
+    });
+
+    const arcLayer = new VectorLayer({
+      source: new VectorSource({
+        features: arcFeatures,
+      }),
+      style: lineStyle,
+    });
+
+
+    map.addLayer(arcLayer);*/
+
   },
   methods: {
     addAirportMarker(map, source, lon, lat, name) {
