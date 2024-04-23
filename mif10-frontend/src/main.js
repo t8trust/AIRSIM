608f0a7a3977ae3f042919c801aa8b6299f51f05
Theo <benard320@gmail.com>
2024-04-23 19:58:33 +0200

    Fix wrong history mode in router

--- mif10-frontend/src/main.js
+++ mif10-frontend/src/main.js
@@ -2,18 +2,21 @@ import { createApp } from 'vue'
 import AppRouter from './AppRouter.vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
-import { createMemoryHistory, createRouter } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import LoginComponent from "./components/admin/LoginComponent.vue"
 import HomeView from './views/HomeView.vue'
 import MapComponent from "./components/MapComponent.vue"
+import DashComp from "./components/admin/DashComp.vue"
+
+
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes: [
     { path: '/', component: HomeView },
     { path: '/map', component: MapComponent },
-    { path: '/login', component: LoginComponent }
-
+    { path: '/login', component: LoginComponent },
+    { path: '/dashboard', component: DashComp }
   ],
 })
 
